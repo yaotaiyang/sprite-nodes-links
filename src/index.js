a805abe9265452dcf147c0b92f4b7b48e6e0edc7
yaotaiyang <yaotaiyang99@gmail.com>
2019-11-26 09:57:24 +0800

    0.0.19 publish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Link from './Link'
 import Stage from './Stage'
 import Node from './Node'
-import { install } from '@spritejs/shapes'
+import { Polyline } from '@spritejs/shapes'
 import * as spritejs from 'spritejs'
-spritejs.use(install)
+spritejs.use(Polyline)
 export { Link, Stage, Node }
